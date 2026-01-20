ae1900f3c9c9ea9f64255d4a40c183d63a95ee0b
kimyuhyun <escnet@naver.com>
2026-01-20 12:30:10 +0900

    @

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 import autoprefixer from "autoprefixer";
 
 export default defineConfig({
+    base: '/igazy_client3/',
     plugins: [react()],
     build: {
         outDir: "./docs",
